ea2f7918d86c4eda5391322f58d782499f75ea93
Keari Eggers <keari@Kearis-MacBook-Air.local>
2017-03-05 11:07:35 -0600

    add comments to example

--- example/index.js
+++ example/index.js
@@ -1,10 +1,11 @@
 const Daphne = require('..')
 const path = require('path')
 
+// instantiation
 const app = new Daphne()
 const port = 3000
 
-app.use(app.static(path.join(__dirname, 'public')))
+app.use(app.static(path.join(__dirname, 'public'))) // grabs everything from public folder and creates routes for all of them
 
 const logger = (req, res) => {
   console.log(`${res.statusCode} ${req.method} request for ${req.url}`)
@@ -12,25 +13,29 @@ const logger = (req, res) => {
 
 const handleNotFound = (req, res) => {
   if (res.statusCode === 404) {
-    res.render('not-found.html')
+    res.render('not-found.html') // route 404s to specific page
   }
 }
 
+// use middleware
 app.use(logger)
 app.use(handleNotFound)
 
+// routes
+app.get('/', (req, res) => {
+  res.render('index.html')
+})
+
 app.get('/hello', (req, res) => {
-  res.send('hola!')
+  res.send('hola!') // sends plain text
 })
 
 app.get('/dog', (req, res) => {
   res.send('public/daphne.jpg')
 })
 
-app.get('/', (req, res) => {
-  res.render('index.html')
-})
-
+// tell daphne to start listening for requests
+// this should always be last
 app.listen(port, () => {
   console.log('Example app listening on port ' + port)
 })
