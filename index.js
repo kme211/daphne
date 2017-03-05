e1927f35838242a2b6111271b7887263eb7de1eb
Keari Eggers <keari@Kearis-MacBook-Air.local>
2017-03-05 10:57:45 -0600

    add non valid type error handling

--- index.js
+++ index.js
@@ -72,6 +72,10 @@ class Daphne {
     let regEx = /\.\w{3,4}$/
     const extensionMatches = filePath.match(regEx)
     let type = extensionMatches ? extensionMatches[0].slice(1) : 'text'
+    if(!isValidType(type)) {
+      let error = new Error(`Daphne: type ${type} is not recognized as a valid type`)
+      return console.log(error.stack)
+    }
     response.writeHead(200, { 'Content-Type': mimeTypes[type] })
     if(type === 'text') {
        response.end(filePath)
